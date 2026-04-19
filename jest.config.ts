96fa97e577640f54cc9653e434a026a16a50b611
junsbabay-pixel <junsbabay@gmail.com>
2026-04-19 20:49:29 +0800

    Add jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({ dir: './' });
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  // Don't auto-mock anything; let test files handle their own mocks
+  automock: false,
+};
+
+export default createJestConfig(config);
